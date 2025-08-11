8c1a4f826e56284100ed40e4ca089c1cb86905b5
Ananth <ananthkrj@gmail.com>
2025-08-11 11:56:56 -0700

    rope kernel done

--- rope/rope.cu
+++ rope/rope.cu
@@ -16,7 +16,7 @@ Workflow:
 // in total B, H, S, D represent the number dimensions of input processed
 // by model, or the total elements
 
-__global__ void rope_kernel(const float* input, float* input,
+__global__ void rope__optimized_kernel(const float* input, float* input,
 const float* cos_cached, const float* sin_cached, int B, int H, int S, int D) {
     // existing logic, elementwise processing
     // this is good for small sequences, debugging, and validatation
@@ -185,10 +185,43 @@ const float* cos_cached, const float* sin_cached, int B, int S, int D, int H) {
     output[y_idx] = x * sin_val + y * cos_val;
 }
 
-void rope_forward() {
+void rope_forward(const float* input, float* output, const float* cos_cached,
+const float* sin_cached, int B, int H, int S, int D) {
     // if the length of the sequence is less than the
     // threshold use the simple kernel
     // otherwise, memory bound scenario then use
     // optimized kernel
     // launch said kernel with blockdim and gridim
+
+    // define total pairs again
+    int total_pairs = B * H * S * (D / 2);
+
+    // choose block size based on problem size
+    // total_pairs when 256 vs 512
+    // understand why these numbers specifically
+    int block_size;
+    if (total_pairs < 1024) {
+        block_size = 256;
+    } else {
+        block_size = 512;
+    }
+
+    int grid_size = (total_pairs + block_size - 1) / block_size;
+
+    // specify dimensions of grid of thread blocks 
+    // and dimensions of each thread block in terms of threads
+    dim3 block(block_size);
+    dim3 grid(grid_size);
+
+    // use shared memory for larger problems/text
+    if (S > 512 && block_size >= 256) {
+        size_t shared_mem_size = 2 * block_size * sizeof(float);
+        // launch kernel
+        shared_rope<<<grid, block, shared_mem_size>>>(input, output,
+        cos_cached, sin_cached, B, H, S, D);
+    } else {
+        rope_optimized_kernel<<<grid, block>>>(input, output, cos_cached,
+        sin_cached, B, H, S, D);
+    }
+    // error checking for kernel launch (find out if better to do in binding or cuda file)
 }
